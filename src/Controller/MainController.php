e0ef7b181a7969419bb9b4a8afc6f93fe6e0b2f4
sergei <mlz.psant@gmail.com>
2022-04-18 10:58:27 +0300

    commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,7 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="mobile_homepage", host="m.example.com")
      */
+
     public function mobileHomepage(): Response
     {
         dd('mobileHomepage');
@@ -21,9 +22,25 @@ class MainController extends AbstractController
      */
     public function homepage(): Response
     {
+        /*
+        $name = 'hello';
+// creates a simple Response with a 200 status code (the default)
+        $response = new Response('Hello '.$name, Response::HTTP_OK);
+
+// creates a CSS-response with a 200 status code
+        $response = new Response('<style> ... </style>');
+        $response->headers->set('Content-Type', 'text/css');
+        return $response;*/
+
+        //$contentsDir = $this->getParameter('kernel.project_dir').'/contents';
+//dd($contentsDir);
+        //return $this->file($this->getParameter('kernel.project_dir').'/var/1.jpg');
+        $articles = ['hello'=>array('slug'=>1,'title'=>2),'hello2'=>array('slug'=>3,'title'=>4)];
+
         $title = 'Main';
         return $this->render('main/index.html.twig', [
-            'title' => $title
+            'title' => $title,
+            'articles' => $articles
         ]);
     }
 }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -12,7 +12,7 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Product')
+->add('Product')
         ;
     }
 
